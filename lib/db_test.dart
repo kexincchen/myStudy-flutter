4b66744f9400c51806c47e0959f0c89600546d8d
Kexin (Clare) Chen <108605930+kexincchen@users.noreply.github.com>
2024-01-07 13:56:50 +0800

    added sqlite

--- lib/db_test.dart
+++ lib/db_test.dart
@@ -1,149 +1,239 @@
-import 'dart:async';
-
-import 'package:flutter/widgets.dart';
-import 'package:path/path.dart';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:http/http.dart' as http;
+import 'package:home_widget/home_widget.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+const String appGroupId = 'group.aiwidget';
+const String iOSWidgetName = 'AI-widget';
 
 void main() async {
-  // Avoid errors caused by flutter upgrade.
-  // Importing 'package:flutter/widgets.dart' is required.
-  WidgetsFlutterBinding.ensureInitialized();
-  // Open the database and store the reference.
-  final database = openDatabase(
-    // Set the path to the database. Note: Using the `join` function from the
-    // `path` package is best practice to ensure the path is correctly
-    // constructed for each platform.
-    join(await getDatabasesPath(), 'doggie_database.db'),
-    // When the database is first created, create a table to store dogs.
-    onCreate: (db, version) {
-      // Run the CREATE TABLE statement on the database.
-      return db.execute(
-        'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
-      );
-    },
-    // Set the version. This executes the onCreate function and provides a
-    // path to perform database upgrades and downgrades.
-    version: 1,
-  );
+  // await dotenv.load();
+  runApp(MyApp());
+}
 
-  // Define a function that inserts dogs into the database
-  Future<void> insertDog(Dog dog) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
-    await db.insert(
-      'dogs',
-      dog.toMap(),
-      conflictAlgorithm: ConflictAlgorithm.replace,
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Generator App',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: MyHomePage(),
     );
   }
+}
 
-  // A method that retrieves all the dogs from the dogs table.
-  Future<List<Dog>> dogs() async {
-    // Get a reference to the database.
-    final db = await database;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
+class _MyHomePageState extends State<MyHomePage> {
+  late final TextEditingController promptController;
+  int selectedNumber = 1;
+  List<String> generatedTexts = [];
+  late bool isLoading;
 
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
-    return List.generate(maps.length, (i) {
-      return Dog(
-        id: maps[i]['id'] as int,
-        name: maps[i]['name'] as String,
-        age: maps[i]['age'] as int,
-      );
+  @override
+  void initState() {
+    super.initState();
+    HomeWidget.setAppGroupId(appGroupId);
+    promptController = TextEditingController();
+    isLoading = false;
+    loadResponsesFromDatabase();
+  }
+
+  @override
+  void dispose() {
+    promptController.dispose();
+    super.dispose();
+  }
+
+  Future<List<String>> getGptResponses(String inputText) async {
+    // final String apiUrl = 'https://api.openai.com/v1/completions';
+    // final response = await http.post(
+    //   Uri.parse(apiUrl),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'Authorization': 'Bearer ${dotenv.env['OPEN_AI_API_KEY']}',
+    //   },
+    //   body: jsonEncode(
+    //     {
+    //       "model": "gpt-3.5-turbo-instruct",
+    //       "prompt": inputText,
+    //       "max_tokens": 250,
+    //       "temperature": 0.8,
+    //     },
+    //   ),
+    // );
+
+    // final data = json.decode(response.body);
+    List<String> responses = ["1. English-French", "2. Chinese-Cantonese"];
+
+    responses = responses
+        .where((response) => response.trim().isNotEmpty)
+        .map((response) => response.replaceFirst(RegExp(r'^\d+\.\s*'), ''))
+        .toList();
+
+    for (String response in responses) {
+      await DatabaseHelper.instance.insert({
+        DatabaseHelper.columnResponse: response,
+      });
+    }
+
+    updateWidget(responses[0]);
+    return responses;
+  }
+
+  Future<void> loadResponsesFromDatabase() async {
+    List<Map<String, dynamic>> queryRows =
+        await DatabaseHelper.instance.queryAllRows();
+    setState(() {
+      generatedTexts = queryRows
+          .map((row) => row[DatabaseHelper.columnResponse] as String)
+          .toList();
     });
   }
 
-  Future<void> updateDog(Dog dog) async {
-    // Get a reference to the database.
-    final db = await database;
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
 
-    // Update the given Dog.
-    await db.update(
-      'dogs',
-      dog.toMap(),
-      // Ensure that the Dog has a matching id.
-      where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [dog.id],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AI Widget'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton<int>(
+              value: selectedNumber,
+              items: [1, 2, 3, 4, 5].map((int value) {
+                return DropdownMenuItem<int>(
+                  value: value,
+                  child: Text('Number $value'),
+                );
+              }).toList(),
+              onChanged: (int? newValue) {
+                setState(() {
+                  selectedNumber = newValue!;
+                });
+              },
+            ),
+            TextField(
+              controller: promptController,
+              decoration: InputDecoration(
+                hintText: 'Caption...',
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            ElevatedButton(
+              onPressed: isLoading
+                  ? null
+                  : () async {
+                      setState(() {
+                        isLoading = true;
+                      });
+
+                      String inputText = promptController.text;
+                      List<String> responses = await getGptResponses(
+                          'Give me $selectedNumber $inputText ');
+
+                      setState(() {
+                        generatedTexts.addAll(responses);
+                        isLoading = false;
+                      });
+                    },
+              child: Text('Generate'),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            isLoading
+                ? CircularProgressIndicator()
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: generatedTexts.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(right: 8.0),
+                                  child: Text('${index + 1}.',
+                                      style:
+                                          TextStyle(fontWeight: FontWeight.bold)),
+                                ),
+                                Expanded(
+                                  child: Text(generatedTexts[index]),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
+      ),
     );
   }
-
-  Future<void> deleteDog(int id) async {
-    // Get a reference to the database.
-    final db = await database;
-
-    // Remove the Dog from the database.
-    await db.delete(
-      'dogs',
-      // Use a `where` clause to delete a specific dog.
-      where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
-      whereArgs: [id],
-    );
-  }
-
-  // Create a Dog and add it to the dogs table
-  var fido = const Dog(
-    id: 0,
-    name: 'Fido',
-    age: 35,
-  );
-
-  await insertDog(fido);
-
-  // Now, use the method above to retrieve all the dogs.
-  print(await dogs()); // Prints a list that include Fido.
-
-  // Update Fido's age and save it to the database.
-  fido = Dog(
-    id: fido.id,
-    name: fido.name,
-    age: fido.age + 7,
-  );
-  await updateDog(fido);
-
-  // Print the updated results.
-  print(await dogs()); // Prints Fido with age 42.
-
-  // Delete Fido from the database.
-  await deleteDog(fido.id);
-
-  // Print the list of dogs (empty).
-  print(await dogs());
 }
 
-class Dog {
-  final int id;
-  final String name;
-  final int age;
+void updateWidget(String response) {
+  HomeWidget.saveWidgetData<String>('headline_description', response);
+  HomeWidget.updateWidget(
+    iOSName: iOSWidgetName,
+  );
+}
 
-  const Dog({
-    required this.id,
-    required this.name,
-    required this.age,
-  });
+class DatabaseHelper {
+  static final _databaseName = "MyDatabase.db";
+  static final _databaseVersion = 1;
+  static final table = 'responses';
+  static final columnId = 'id';
+  static final columnResponse = 'response';
 
-  // Convert a Dog into a Map. The keys must correspond to the names of the
-  // columns in the database.
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-    };
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
   }
 
-  // Implement toString to make it easier to see information about
-  // each dog when using the print statement.
-  @override
-  String toString() {
-    return 'Dog{id: $id, name: $name, age: $age}';
+  _initDatabase() async {
+    String path = join(await getDatabasesPath(), _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,
+            $columnResponse TEXT NOT NULL
+          )
+          ''');
+  }
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryAllRows() async {
+    Database db = await instance.database;
+    return await db.query(table);
   }
 }
